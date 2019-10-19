cc0a76311513b68d44a311a799d522bd64145820
Mikołaj Ewald <mikolajewald@gmail.com>
2019-10-19 14:58:02 +0200

    komentarz

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const arrow = document.querySelector("#arrow");
 const sidebar = document.querySelector("nav");
 arrow.addEventListener("click", () => {
-    sidebar.classList.add("hide");
-    arrow.classList.add("rotate");
+    sidebar.classList.toggle("hide");
+    arrow.classList.toggle("rotate");
 })
