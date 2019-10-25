9ea3b02cf4a256c6936530aa268f248eaae021e4
Mikołaj Ewald <mikolajewald@gmail.com>
2019-10-25 22:34:01 +0200

    kom

--- links/form.js
+++ links/form.js
@@ -1,7 +1,37 @@
-let i = 1;
+let i = 2;
 const next = document.querySelector(".dalej");
-next.addEventListener("click", () => {
+let form = document.querySelectorAll(".one");
+let form2 = document.querySelectorAll(".two");
+next.addEventListener("click", (e) => {
+    e.preventDefault();
     let div = document.querySelector(`#progress div:nth-child(${i})`);
     div.classList.add("active");
-    i++;
+    if (i == 2) {
+    form.forEach(element => {
+        element.classList.add("display");
+    });
+    form2.forEach(element => {
+        element.classList.remove("display");
+    });
+    }
+    if (i < 5) {
+        i++;
+    }
+});
+const back = document.querySelector(".cofnij");
+back.addEventListener("click", (e) => {
+    e.preventDefault();
+    let div = document.querySelector(`#progress div:nth-child(${i})`);
+    div.classList.remove("active");
+    if (i == 3) {
+    form2.forEach(element => {
+        element.classList.add("display");
+    });
+    form.forEach(element => {
+        element.classList.remove("display");
+    });   
+    }
+    if (i > 2) {
+        i--;
+    }
 });
--- podstrony/signupP.php
+++ podstrony/signupP.php
@@ -15,13 +15,18 @@
             <div></div>
             <div><p>3</p></div>
         </div>
+        <!--PANIE EGZAMINATORZE KURWA TU SĄ MOJE A I B DO NICH WPISUJE DANE, PISZĘ DO DLA PANA, MIŁEGO DNIA BRKI TO-->
         <form action="" method="post">
-            <label for="email">Email</label><br>
-            <input type="text" name="email" class="form one"><br>
-            <label for="password">Hasło</label><br>
-            <input type="password" name="password" class="form one"><br>
-            <label for="password2">Potwierdź hasło</label><br>
-            <input type="password" name="password2" class="form one"><br>
+            <label for="email" class="one">Email</label>
+            <input type="text" name="email" class="form one">
+            <label for="password" class="one">Hasło</label>
+            <input type="password" name="password" class="form one">
+            <label for="password2" class="one">Potwierdź hasło</label>
+            <input type="password" name="password2" class="form one">
+            <label for="nick" class="display two">Nick</label>
+            <input type="text" name="nick" class="form two display">
+            <label for="avatar" class="display two">Zdjęcie profilowe</label>
+            <input type="file" accept="image/x-png,image/gif,image/jpeg" name="avatar" class="avatar two display">
             <button class="przycisk cofnij" >cofnij</button>
             <button class="przycisk dalej">dalej</button>
         </form>
