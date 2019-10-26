2c88712669969139a225cf905a0af38201a4ae97
Mikołaj Ewald <mikolajewald@gmail.com>
2019-10-26 14:38:07 +0200

    kom

--- links/form.js
+++ links/form.js
@@ -1,29 +1,53 @@
-let i = 2;
+let i = 1;
 const form1 = document.querySelector(".one");
 const form2 = document.querySelector(".two");
+const form3 = document.querySelector(".three")
 const next = document.querySelector(".dalej");
 const back = document.querySelector(".cofnij");
-
+const submit = document.querySelector(".send");
 next.addEventListener("click", (e) => {
     e.preventDefault();
-    let div = document.querySelector(`#progress div:nth-child(${i})`);
-    div.classList.add("active");
+    if (i < 4) i++;
+    let div1 = document.querySelector(`#progress div:nth-child(${i})`);
+    let div2 = document.querySelector(`#progress div:nth-child(${i+1})`);
+    div1.classList.add("active");
+    div2.classList.add("active");
     if (i == 2) {
         form1.classList.add("display");
         form2.classList.remove("display");
     }
-    if (i < 5) i++;
+    if (i == 4) {
+        form2.classList.add("display");
+        form3.classList.remove("display");
+        next.classList.add("display"); 
+        submit.classList.remove("display");
+    }
+    if (i < 4) i++;
 });
 
 back.addEventListener("click", (e) => {
     e.preventDefault();
-    let div = document.querySelector(`#progress div:nth-child(${i})`);
-    div.classList.remove("active");
-    if (i == 3) {
+    if (i != 1) {
+    if (i == 3) i--;
+    let div1 = document.querySelector(`#progress div:nth-child(${i})`);
+    let div2 = document.querySelector(`#progress div:nth-child(${i + 1})`);
+    div1.classList.remove("active");
+    div2.classList.remove("active");
+    if (i == 2) {
         form2.classList.add("display");
         form1.classList.remove("display");
-    }
-    if (i > 2) {
         i--;
     }
-})
+    if (i == 4) {
+        form3.classList.add("display");
+        form2.classList.remove("display");
+        submit.classList.add("display");
+        next.classList.remove("display");
+    }
+    if (i > 3) {
+        i -= 2;
+    }
+    } else {
+
+    }
+});
--- podstrony/signupP.php
+++ podstrony/signupP.php
@@ -15,32 +15,42 @@
             <div></div>
             <div><p>3</p></div>
         </div>
-        <form action="" method="post">
+        <form action="index.php" method="post">
             <div class="one">
                 <label for="email">Email</label>
-                <input type="text" name="email" class="form">
+                <input type="text" name="email" placeholder="podaj e-mail">
                 <label for="password" >Hasło</label>
-                <input type="password" name="password" class="form">
+                <input type="password" name="password" placeholder="wpisz hasło">
                 <label for="password2" class="one">Potwierdź hasło</label>
-                <input type="password" name="password2" class="form">
+                <input type="password" name="password2" placeholder="potwierdź hasło">
             </div>
-            <!-- 
-            1. felxboxy
-            2. dać wszedzie klase display, w sensie na sekcje
-            3, przerobić tego cssa i htmla
-            -->
+
             <div class="display two"> 
                 <label for="nick">Nick</label>
-                <input type="text" name="nick" class="form">
-                <div class="profile-picture"></div>
-                <label for="avatar" class="fileContainer two">Zdjęcie profilowe
-                <input type="file" accept="image/x-png,image/gif,image/jpeg" name="avatar" class="avatar">
-                </label>
+                <input type="text" name="nick">
+                <div>
+                    <div class="profile-picture"></div>
+                        <label for="avatar" class="fileContainer two">Wybierz zdjęcie profilowe
+                            <input type="file" accept="image/x-png,image/gif,image/jpeg" name="avatar" class="avatar">
+                        </label>
+                </div>
             </div>
-            
+
+            <div class="display three">
+                <label for="language">Wybierz język</label>
+                <select name="language">
+                    <option value="English">Angielski</option>
+                    <option value="German">Niemiecki</option>
+                    <option value="French">Francuski</option>
+                </select>
+                <label for="oSobie">Kilka zdań o sobie</label>
+                <textarea name="oSobie" cols="50" rows="10" wrap="off">
+                </textarea>
+            </div>
             <div>
                 <button class="przycisk cofnij" >cofnij</button>
                 <button class="przycisk dalej">dalej</button>
+                <input type="submit" class="przycisk send display" value="Zarejestruj">
             </div>
         </form>
     </section>
