2f92e6f7caa4b1e6a43c44778deac9759ee25b4d
Mikołaj Ewald <mikolajewald@gmail.com>
2019-11-06 13:43:33 +0100

    fiszkipodstrona

--- podstrony/flashcardList.php
+++ podstrony/flashcardList.php
@@ -1,14 +1,5 @@
     <div id="flashcardList">
         <div>
-            <p>Ulubione fiszki</p>
-            <div>
-                <div>
-                    <img src="" alt="">
-                    <p>Nazwa zestawu</p>
-                </div>
-            </div>
-        </div>
-        <div>
             <p>Wszystkie fiszki</p>
             <div>
                 <div>
