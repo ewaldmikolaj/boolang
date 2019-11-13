fcc97700ba58df0257b941497754700608536d71
Mikołaj Ewald <mikolajewald@gmail.com>
2019-11-13 23:33:55 +0100

    rejestracja

--- links/form.js
+++ links/form.js
@@ -1,53 +1,51 @@
-let i = 1;
-const form1 = document.querySelector(".one");
-const form2 = document.querySelector(".two");
-const form3 = document.querySelector(".three")
+let i = 0;
+var form1 = document.querySelector(".one");
+var form2 = document.querySelector(".two");
+var form3 = document.querySelector(".three")
 const next = document.querySelector(".dalej");
 const back = document.querySelector(".cofnij");
 const submit = document.querySelector(".send");
 next.addEventListener("click", (e) => {
     e.preventDefault();
-    if (i < 4) i++;
-    let div1 = document.querySelector(`#progress div:nth-child(${i})`);
-    let div2 = document.querySelector(`#progress div:nth-child(${i+1})`);
-    div1.classList.add("active");
-    div2.classList.add("active");
-    if (i == 2) {
+    if (form2.classList == "two") {
+        let div1 = document.querySelector(`#progress div:nth-child(4)`);
+        let div2 = document.querySelector(`#progress div:nth-child(5)`);
+        div1.classList.add("active");
+        div2.classList.add("active");
+        form2.classList.add("display");
+        form3.classList.remove("display");
+        next.classList.add("display");
+        submit.classList.remove("display");
+        i = 0;
+    }
+    if (form1.classList != "one display") {
+        let div1 = document.querySelector(`#progress div:nth-child(2)`);
+        let div2 = document.querySelector(`#progress div:nth-child(3)`);
+        div1.classList.add("active");
+        div2.classList.add("active");
         form1.classList.add("display");
         form2.classList.remove("display");
     }
-    if (i == 4) {
-        form2.classList.add("display");
-        form3.classList.remove("display");
-        next.classList.add("display"); 
-        submit.classList.remove("display");
-    }
-    if (i < 4) i++;
 });
 
 back.addEventListener("click", (e) => {
     e.preventDefault();
-    if (i != 1) {
-    if (i == 3) i--;
-    let div1 = document.querySelector(`#progress div:nth-child(${i})`);
-    let div2 = document.querySelector(`#progress div:nth-child(${i + 1})`);
-    div1.classList.remove("active");
-    div2.classList.remove("active");
-    if (i == 2) {
-        form2.classList.add("display");
-        form1.classList.remove("display");
-        i--;
-    }
-    if (i == 4) {
+    if (form2.classList == "two display" && i == 0) {
+        let div1 = document.querySelector(`#progress div:nth-child(4)`);
+        let div2 = document.querySelector(`#progress div:nth-child(5)`);
+        div1.classList.remove("active");
+        div2.classList.remove("active");
         form3.classList.add("display");
         form2.classList.remove("display");
-        submit.classList.add("display");
-        next.classList.remove("display");
-    }
-    if (i > 3) {
-        i -= 2;
-    }
-    } else {
-
+        submit.classList.add("display");    
+        next.classList.remove("display"); 
+    } else if (form1.classList == "one display") {
+        let div1 = document.querySelector(`#progress div:nth-child(2)`);
+        let div2 = document.querySelector(`#progress div:nth-child(3)`);
+        div1.classList.remove("active");
+        div2.classList.remove("active");
+        form2.classList.add("display");
+        form1.classList.remove("display");
+        i++;
     }
 });
--- podstrony/signupP.php
+++ podstrony/signupP.php
@@ -1,59 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <section id="right">
-        <div id="progress">
-            <div class="active"><p>1</p></div>
-            <div></div>
-            <div><p>2</p></div>
-            <div></div>
-            <div><p>3</p></div>
+<section id="right">
+    <div id="progress">
+        <div class="active"><p>1</p></div>
+        <div></div>
+        <div><p>2</p></div>
+        <div></div>
+        <div><p>3</p></div>
+    </div>
+    <form action="index.php" method="post">
+        <div class="one">
+            <label for="email">Email</label>
+            <input type="text" name="email" placeholder="podaj e-mail">
+            <label for="password" >Hasło</label>
+            <input type="password" name="password" placeholder="wpisz hasło">
+            <label for="password2" class="one">Potwierdź hasło</label>
+            <input type="password" name="password2" placeholder="potwierdź hasło">
         </div>
-        <form action="index.php" method="post">
-            <div class="one">
-                <label for="email">Email</label>
-                <input type="text" name="email" placeholder="podaj e-mail">
-                <label for="password" >Hasło</label>
-                <input type="password" name="password" placeholder="wpisz hasło">
-                <label for="password2" class="one">Potwierdź hasło</label>
-                <input type="password" name="password2" placeholder="potwierdź hasło">
-            </div>
 
-            <div class="display two"> 
-                <label for="nick">Nick</label>
-                <input type="text" name="nick">
-                <div>
-                    <div class="profile-picture"></div>
-                        <label for="avatar" class="fileContainer two">Wybierz zdjęcie profilowe
-                            <input type="file" accept="image/x-png,image/gif,image/jpeg" name="avatar" class="avatar">
-                        </label>
-                </div>
-            </div>
-
-            <div class="display three">
-                <label for="language">Wybierz język</label>
-                <select name="language">
-                    <option value="English">Angielski</option>
-                    <option value="German">Niemiecki</option>
-                    <option value="French">Francuski</option>
-                </select>
-                <label for="oSobie">Kilka zdań o sobie</label>
-                <textarea name="oSobie" cols="50" rows="10" wrap="off">
-                </textarea>
-            </div>
+        <div class="two display "> 
+            <label for="nick">Nick</label>
+            <input type="text" name="nick">
             <div>
-                <button class="przycisk cofnij" >cofnij</button>
-                <button class="przycisk dalej">dalej</button>
-                <input type="submit" class="przycisk send display" value="Zarejestruj">
+                <div class="profile-picture"></div>
+                    <label for="avatar" class="fileContainer two">Wybierz zdjęcie profilowe
+                        <input type="file" accept="image/x-png,image/gif,image/jpeg" name="avatar" class="avatar">
+                    </label>
             </div>
-        </form>
-    </section>
-    <script src="links/form.js"></script>
-</body>
-</html>
+        </div>
+
+        <div class="three display">
+            <label for="language">Wybierz język</label>
+            <select name="language">
+                <option value="English">Angielski</option>
+                <option value="German">Niemiecki</option>
+                <option value="French">Francuski</option>
+            </select>
+            <label for="oSobie">Kilka zdań o sobie</label>
+            <textarea name="oSobie" rows="10" wrap="off">
+            </textarea>
+        </div>
+        <div>
+            <button class="przycisk cofnij" >cofnij</button>
+            <button class="przycisk dalej">dalej</button>
+            <input type="submit" class="przycisk send display" value="Zarejestruj">
+        </div>
+    </form>
+</section>
+<script src="links/form.js"></script>
