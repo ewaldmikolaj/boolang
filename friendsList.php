3c456cafbd9412ca0905ce4408a4938a50888383
Mikołaj Ewald <mikolajewald@gmail.com>
2019-11-26 00:16:20 +0100

    rwd

--- friendsList.php
+++ friendsList.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="links/style.css">
     <link rel="stylesheet" href="links/media.css">
     <title>BooLang | Znajomi</title>
--- learn.php
+++ learn.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>BooLang | Nauka</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="links/style.css">
     <link rel="stylesheet" href="links/media.css">
-
+    <title>BooLang | Nauka</title>
 </head>
 <body>
     <?php
