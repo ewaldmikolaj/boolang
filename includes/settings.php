e3fa1c8bc4394ab9d9d01005e6a92f264bde9f9b
Mikołaj Ewald <mikolajewald@gmail.com>
2020-01-21 00:59:47 +0100

    settings rdy

--- includes/settings.php
+++ includes/settings.php
@@ -2,22 +2,31 @@
 
 if (isset($_POST['stngs-submit'])) {
 
+    if (!isset($_SESSION)){
+        session_start();
+    }
+
     require 'dbh.php';
 
     if (empty($_POST['username']) && empty($_POST['email']) && empty($_POST['description'])) {
         header('Location: ./../pages/settingsmain.php?empty');
     } else {
-        if (isset($_POST['username'])) {
+        if (!empty($_POST['username'])) {
             $username = $_POST['username'];
             $sql = "UPDATE uzytkownik SET Nazwa = '$username' WHERE IDuzytkownika = '".$_SESSION['userId']."';";
             mysqli_query($conn, $sql);
         }
-        if (isset($_POST['email'])) {
+        if (!empty($_POST['email'])) {
             $email = $_POST['email'];
+            $sql2 = "UPDATE uzytkownik SET Email = '$email' WHERE IDuzytkownika = '".$_SESSION['userId']."';";
+            mysqli_query($conn, $sql2);
         }
-        if (isset($_POST['description'])) {
+        if (!empty($_POST['description'])) {
             $description = $_POST['description'];
+            $sql3 = "UPDATE uzytkownik SET Opis = '$description' WHERE IDuzytkownika = '".$_SESSION['userId']."';";
+            mysqli_query($conn, $sql3);
         }
+        header('Location: ./../pages/settingsmain.php?zmieniono');
     }
 
 } else {
